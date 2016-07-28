ac89f71a13822d617b9b387ac53a9978aa656cc2
chaibiskut <trivediniki94@gmail.com>
2016-07-28 17:49:43 +0530

    Add files via upload
    rgrtgrtbrtth

--- css.php
+++ css.php
@@ -1,6 +1,15 @@
-<link href="css/bootstrap.min.css" rel="stylesheet">
- <!-- <link rel="stylesheet" href="http://path/to/font-awesome/css/font-awesome.min.css"> -->
- <link href="css/custom.css" rel="stylesheet">
+<link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="<?php echo base_url();?>css/dataTables.bootstrap.min.css" rel="stylesheet"> -->
+
+ <link href="<?php echo base_url();?>css/custom.css" rel="stylesheet">
  <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+ <!-- <link href="css/footable/footable.core.css" rel="stylesheet" /> -->
+ <link href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+ <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/datatables.min.css"/>
+
+  <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"> 
+     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/0.9.0/bootstrap-progressbar.js"> 
+<link href=https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/0.9.0/bootstrap-progressbar.min.js> 
 
  
